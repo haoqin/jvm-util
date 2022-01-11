501271934657990a135923fd585879a61dc22605
hao.qin <hqin@liyutech.com>
2022-01-11 15:32:25 -0600

    v0.1.1: Added logic to compare schemat of json values

--- common/src/main/scala/com/liyutech/common/JsonUtil.scala
+++ common/src/main/scala/com/liyutech/common/JsonUtil.scala
@@ -1,6 +1,6 @@
 package com.liyutech.common
 
-import io.circe.Json
+import io.circe.{Json, JsonObject}
 
 object JsonUtil {
 
@@ -11,4 +11,120 @@ object JsonUtil {
   def tuple2ToJson[A, B](input: Seq[(A, B)])(fn: (A, B) => Json): Json = {
     Json.fromValues(input.map { case (a, b) => fn(a, b) })
   }
+
+  def jsonType(json: Json): String = {
+    if (json.isObject) {
+      "object"
+    }
+    else if (json.isArray) {
+      "array"
+    }
+    else if (json.isBoolean) {
+      "boolean"
+    }
+    else if (json.isString) {
+      "string"
+    }
+    else if (json.isNumber) {
+      "number"
+    }
+    else if (json.isNull) {
+      "null"
+    }
+    else {
+      "unknown"
+    }
+  }
+
+
+  implicit class DynamicJsonSchemaValidator(json: Json) {
+    private def schemaMismatches(matchedKeys: Set[String], obj: JsonObject, anotherObj: JsonObject): Set[String] = {
+      matchedKeys.flatMap { key =>
+        // Option.get is guaranteed to be safe here.
+        obj(key).get.schemaMismatches(anotherObj(key).get)
+      }
+    }
+
+    private def schemaMismatches(matchedKeys: Set[String], newKeys: Set[String], obj: JsonObject, anotherObj: JsonObject): Set[String] = {
+      matchedKeys.flatMap { key =>
+        // Option.get is guaranteed to be safe here because the keys are shared by both objects.
+        obj(key).get.schemaMismatches(anotherObj(key).get)
+      }
+    }
+
+    // Compare the schemata of two Json values and return all mismatches. Since JSON only has a small set of data types,
+    // the comparison will be not complete. For example, LocalDateTime is often expressed as a JSON string, so the
+    // schema comparison against LocalDateTime against string will reveal no difference.
+    def schemaMismatches(anotherJson: Json): Set[String] = {
+      if (json.isBoolean) {
+        if (anotherJson.isBoolean) {
+          Set.empty[String]
+        }
+        else {
+          Set(s"A boolean does not match ${jsonType(anotherJson)}")
+        }
+      }
+      else if (json.isNumber) {
+        if (anotherJson.isNumber) {
+          Set.empty[String]
+        }
+        else {
+          Set(s"A number does not match ${jsonType(anotherJson)}")
+        }
+      }
+      else if (json.isString) {
+        if (anotherJson.isString) {
+          Set.empty[String]
+        }
+        else {
+          Set(s"A string does not match ${jsonType(anotherJson)}")
+        }
+      }
+      else if (json.isNull) {
+        if (anotherJson.isNull) {
+          Set.empty[String]
+        }
+        else {
+          Set(s"A null type does not match ${jsonType(anotherJson)}")
+        }
+      }
+      else if (json.isObject) {
+        if (anotherJson.isObject) {
+          val obj = json.asObject.get
+          val anotherObj = anotherJson.asObject.get
+          val objKeys: Set[String] = obj.keys.toSet
+          val anotherObjKeys: Set[String] = anotherObj.keys.toSet
+          val matchedKeys: Set[String] = objKeys.intersect(anotherObjKeys)
+          val missingKeys: Set[String] = objKeys.filterNot(anotherObjKeys)
+          val newKeys: Set[String] = anotherObjKeys.filterNot(objKeys)
+          schemaMismatches(matchedKeys, obj, anotherObj) ++
+            missingKeys.map(key => s"Missing key: $key") ++
+            newKeys.map(key => s"Unexpected new key: $key")
+        }
+        else {
+          Set(s"Cannot compare a json object to ${jsonType(anotherJson)}.")
+        }
+      }
+      else if (json.isArray) {
+        if (anotherJson.isArray) {
+          val aMismatchB = for { a <- json.asArray.get
+            b <- anotherJson.asArray.get
+          } yield a.schemaMismatches(b)
+
+          val bMismatchA = for {
+            b <- anotherJson.asArray.get
+            a <- json.asArray.get
+          } yield b.schemaMismatches(a)
+          aMismatchB.flatten.toSet ++ bMismatchA.flatten.toSet
+        }
+        else {
+          Set(s"Cannot compare a json object to ${jsonType(anotherJson)}.")
+        }
+      }
+      else {
+        Set(s"Cannot compare ${jsonType(json)} to ${jsonType(anotherJson)}.")
+      }
+    }
+  }
+
 }
---
+++ common/src/test/scala/com/liyutech/common/JsonUtilSpec.scala
@@ -0,0 +1,55 @@
+package com.liyutech.common
+
+import com.liyutech.common.CommonUtil.TestResourceDir
+import io.circe.Json
+import org.scalatest.flatspec.AsyncFlatSpec
+
+class JsonUtilSpec extends AsyncFlatSpec {
+
+  import JsonUtil._
+  import io.circe.parser._
+
+  "JsonUtil" should "validate simple value types" in {
+    val happyPaths = Json.Null.schemaMismatches(Json.Null).isEmpty &&
+      Json.fromInt(1).schemaMismatches(Json.fromInt(1)).isEmpty
+    val errorPaths = Set(Json.Null, Json.fromBoolean(true)).forall(Json.fromInt(1).schemaMismatches(_).nonEmpty) &&
+      Set(Json.fromInt(1), Json.fromBoolean(true)).forall(Json.Null.schemaMismatches(_).nonEmpty)
+
+    assert(happyPaths && errorPaths)
+  }
+
+  "JsonUtil" should "validate simple json objects" in {
+
+    val happyPath = for {
+      obj <- parse("""{"a": 1}""")
+      matchedObj <- parse("""{"a": 10}""")
+    } yield obj.schemaMismatches(matchedObj)
+
+    val simpleErrorPath = for {
+      obj <- parse("""{"a": 1}""")
+      matchedObj <- parse("""{"b": 10}""")
+    } yield obj.schemaMismatches(matchedObj)
+    val expectedErrors: Set[String] = Set("Missing key: a", "Unexpected new key: b")
+    assert(happyPath.map(_.isEmpty).isRight && simpleErrorPath.map(_ == expectedErrors) == Right(true))
+  }
+
+  "JsonUtil" should "validate complex json objects" in {
+    val sourceSchema = parse(CommonUtil.readFileAsString("DynamicJsonSchema.json", TestResourceDir.replace("jvm-util", "jvm-util/common")))
+    val happyPath = for {
+      schema <- sourceSchema
+    } yield schema.schemaMismatches(schema)
+
+    val mismatchedSchema = parse(CommonUtil.readFileAsString("MismatchedJsonSchema.json", TestResourceDir.replace("jvm-util", "jvm-util/common")))
+    val errorPath = for {
+      schema <- sourceSchema
+      mismatched <- mismatchedSchema
+    } yield schema.schemaMismatches(mismatched)
+
+    println(errorPath)
+
+    // The number of errors compare two schemas should always be an even number because the errors are reciprocal.
+    val assertEvenErrorSizes: Set[String] => Boolean = errors => errors.nonEmpty && errors.size % 2 == 0
+    assert(happyPath.map(_.isEmpty) == Right(true) && errorPath.map(assertEvenErrorSizes) == Right(true))
+  }
+
+}
